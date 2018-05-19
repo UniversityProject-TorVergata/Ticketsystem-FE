b04eb92a3c0548b75504f73e54abf41d486a27e0
Andrea Pera <perozzo92@hotmail.it>
2018-05-19 20:44:26 +0200

    Piccole modifiche negli dettagli utente.

--- app/templates/accountThirdPartyCustomer/accountThirdPartyCustomerController.js
+++ app/templates/accountThirdPartyCustomer/accountThirdPartyCustomerController.js
@@ -4,19 +4,27 @@ angular.module('ticketsystem.accountThirdPartyCustomer', ['ngRoute'])
 
     .controller('accountThirdPartyCustomerCtrl', function ($scope, restService, httpService,$location) {
 
+        $scope.disabledButton = true;
+        $scope.disabledReadonly = true;
 
-        $scope.changeAccountInformation = function () {
-
-
-
-
-        }
-
-        $scope.deleteAccount = function () {
+        $scope.changeAccountInformation = function() {
 
 
 
 
+        };
+
+        $scope.deleteAccount = function() {
+
+
+
+
+        };
+
+        $scope.disabledButtonHandler = function() {
+
+            $scope.disabledButton = !($scope.disabledButton);
+            $scope.disabledReadonly = !($scope.disabledReadonly);
         }
 
 
