f85c7c0381242908358af5bd51f6f113429d78bb
Alessio Nazio <alessio.nazio@gmail.com>
2018-05-19 18:32:33 +0200

    mocks - Used for test purpose with JSONs model.service - GET Product and inject in select item

--- app/templates/createTicket/createTicketController.js
+++ app/templates/createTicket/createTicketController.js
@@ -23,16 +23,21 @@ angular.module('ticketsystem.createTicket', ['ngRoute'])
             //TODO convertire bene
             //TODO Qualsiasi cosa tu debba fare usa moment.js per le date. è già nel package.json e importato in angular
             $scope.ticket.created_at = date;
+            $scope.ticket.state = "CREATED";
+
+            // TODO correggere 'sta zozzata
+            $scope.ticket.target = 1
             console.log($scope.ticket);
-            /*httpService.post(restService.createTicket, $scope.ticket)
+            httpService.post(restService.createTicket, $scope.ticket)
                 .then(function (data) {
                         console.log(data)
                         //$location.path('/table')
                     },
                     function(err){
                         $scope.errorMessage = "error!"
-                    })*/
-        }
+                    })
+        };
+
         $scope.selectedFile = function (event) {
             util.getBase64(event.target.files[0]).then(result => {
                 console.log(result)
