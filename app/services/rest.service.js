3f20447cb8eee2f8b5c3a19ed1baa188135adb2a
Alessio Di Luzio <alessio.diluzio94@gmail.com>
2018-05-20 22:51:57 +0200

    Implementato il Login , il service login.service.js ora mantiene i dati dell'utente loggato. Implementato il ritiro o reinserimento di un prodotto dal sistema.

--- app/services/rest.service.js
+++ app/services/rest.service.js
@@ -6,7 +6,7 @@ angular.module('ticketsystem.restService', [])
 
     .service('restService', function (CONNECTION) {
         return {
-            //"login": CONNECTION.url + '/loginUser',
+            "login": CONNECTION.url + '/registered_user/login',
             //"signup": CONNECTION.url + '/createUser',
             "createTicket": CONNECTION.url + '/ticket',
             "createUser": CONNECTION.url + '/registered_user',
@@ -31,6 +31,25 @@ angular.module('ticketsystem.restService', [])
                     "url": url,
                     "headers": head,
                     "data": data
+                });
+            },
+            put: function (url, data, header) {
+                var head = "";
+                if (header) head = header;
+                return $http({
+                    "method": "PUT",
+                    "url": url,
+                    "headers": head,
+                    "data": data
+                });
+            },
+            delete: function (url , header) {
+                var head = "";
+                if (header) head = header;
+                return $http({
+                    "method": "DELETE",
+                    "url": url,
+                    "headers": head
                 });
             }
 
--- app/templates/createProduct/createProductController.js
+++ app/templates/createProduct/createProductController.js
@@ -50,6 +50,58 @@ angular.module('ticketsystem.createProduct', ['ngRoute'])
             });
         };
 
+        $scope.removeProduct = function (index) {
+
+            var removedProduct = $scope.items[index];
+            var id = removedProduct.id;
+            var postData = {
+                "id" : id,
+
+            }
+            httpService.put(restService.createProduct+"/retire/"+id,postData).then(function (response) {
+                // turn on flag for post successfully
+                $scope.items[index].productState = "RETIRED";
+
+            }, function error(response) {
+
+                window.alert("Error : " + response.statusText);
+
+            });
+
+
+        };
+
+        $scope.reabProduct = function (index) {
+
+            var removedProduct = $scope.items[index];
+            var id = removedProduct.id;
+            var postData = {
+                "id" : id,
+
+            }
+            httpService.put(restService.createProduct+"/reab/"+id,postData).then(function (response) {
+                // turn on flag for post successfully
+                $scope.items[index].productState = "ACTIVE";
+
+            }, function error(response) {
+
+                window.alert("Error : " + response.statusText);
+
+            });
+
+
+        };
+
+
+
+        $scope.isRetired = function(index){
+            if($scope.items[index].productState == "RETIRED")
+                return true;
+            else
+                return false;
+
+        };
+
 
 
 
