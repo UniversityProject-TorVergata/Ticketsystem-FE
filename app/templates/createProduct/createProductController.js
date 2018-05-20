714fac9fa478855fd91b383dc446622f6bf7887f
Alessio Di Luzio <alessio.diluzio94@gmail.com>
2018-05-20 11:59:46 +0200

    Realizzata l'interfaccia per la creazione di un prodotto e il relativo controller.

--- app/templates/createProduct/createProductController.js
+++ app/templates/createProduct/createProductController.js
@@ -3,9 +3,30 @@
 angular.module('ticketsystem.createProduct', ['ngRoute'])
 
     .controller('createProductCtrl', function ($scope, restService, httpService,$location) {
+        var url = "http://localhost:8200/ticketsystem/product/";
+
+        $scope.product = {};
+        var config = {
+            headers : {
+                'Content-Type': 'application/json',
 
 
+            }
+        };
 
+        $scope.creationProduct = function(){
+
+            httpService.post(url,$scope.product,config).then(function (response) {
+                // turn on flag for post successfully
+                $scope.risposta="Prodotto inserito " + response.data;
+
+            }, function error(response) {
+                $scope.risposta = "Error Status: " +  response.statusText;
+
+            });
+
+
+        };
 
 
 
