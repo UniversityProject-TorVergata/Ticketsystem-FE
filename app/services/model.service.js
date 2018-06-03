f07aca1df737c911055afdeb5cf92e2eb3a41018
Alessio Nazio <alessio.nazio@gmail.com>
2018-06-03 17:00:12 +0200

    Added comments

--- app/services/model.service.js
+++ app/services/model.service.js
@@ -1,7 +1,7 @@
 angular.module('ticketsystem.modelService', [])
     .service('model', function (httpService, restService) {
             return {
-                // TODO Rimuovere
+                // TODO Rimuovere?
                 getProducts() {
                     return new Promise(function (resolve, reject) {
                         httpService.get('./mocks/products.json').then(
@@ -15,6 +15,10 @@ angular.module('ticketsystem.modelService', [])
                     })
                 },
 
+                /**
+                 *  Function gets the targets from the database.
+                 *  @returns {Promise}  targets list
+                 */
                 getTargets() {
                     return new Promise(function (resolve, reject) {
                         //  HTTP GET
@@ -27,6 +31,11 @@ angular.module('ticketsystem.modelService', [])
                             });
                     })
                 },
+
+                /**
+                 *  Function gets the targets from the mock.
+                 *  @returns {Promise} Source Types list
+                 */
                 getSourceTypes() {
                     return new Promise(function (resolve, reject) {
                         httpService.get('./mocks/source_types.json').then(
@@ -38,8 +47,12 @@ angular.module('ticketsystem.modelService', [])
                                 resolve([])
                             })
                     })
-                }
-                ,
+                },
+
+                /**
+                 *  Function gets the TeamLeaders from the database.
+                 *  @returns {Promise} Team Leaders list
+                 */
                 getTeams() {
                     return new Promise(function (resolve, reject) {
                         httpService.get(restService.teamLeader).then(
@@ -47,12 +60,16 @@ angular.module('ticketsystem.modelService', [])
                                 resolve(teams.data);
                             },
                             function (error) {
-                                console.error("Error loading Products")
+                                console.error("Error loading Team Leaders")
                                 resolve([])
                             })
                     })
-                }
-                ,
+                },
+
+                /**
+                 *  Function gets the tags from the mock.
+                 *  @returns {Promise} Tags list
+                 */
                 getTags() {
                     return new Promise(function (resolve, reject) {
                         httpService.get('./mocks/tags.json').then(
@@ -64,8 +81,12 @@ angular.module('ticketsystem.modelService', [])
                                 resolve([])
                             })
                     })
-                }
-                ,
+                },
+
+                /**
+                 *  Function gets the categories from the mock.
+                 *  @returns {Promise} Categories list
+                 */
                 getCategories() {
                     return new Promise(function (resolve, reject) {
                         httpService.get('./mocks/categories.json').then(
@@ -80,6 +101,11 @@ angular.module('ticketsystem.modelService', [])
                     })
 
                 },
+
+                /**
+                 *  Function gets the visibility values from the mock.
+                 *  @returns {Promise} Visibility values list
+                 */
                 getVisibilities() {
                     return new Promise(function (resolve, reject) {
                         httpService.get('./mocks/visibility.json').then(
@@ -87,13 +113,18 @@ angular.module('ticketsystem.modelService', [])
                                 resolve(visibilities.data);
                             },
                             function (error) {
-                                console.error("Error loading Categories")
+                                console.error("Error loading Visibilities")
                                 resolve([])
                             }
                         )
                     })
 
                 },
+
+                /**
+                 *  Function gets the ticket types from the mock.
+                 *  @returns {Promise} Ticket types list
+                 */
                 getTicketTypes() {
                     return new Promise(function (resolve, reject) {
                         httpService.get('./mocks/ticket_type.json').then(
@@ -107,6 +138,11 @@ angular.module('ticketsystem.modelService', [])
                         )
                     })
                 },
+
+                /**
+                 *  Function gets the priority values from the mock.
+                 *  @returns {Promise} Priority values list
+                 */
                 getPriority() {
                     return new Promise(function (resolve, reject) {
                         httpService.get('./mocks/priority.json').then(
