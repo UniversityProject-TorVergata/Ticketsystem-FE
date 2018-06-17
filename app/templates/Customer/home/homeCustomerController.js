eb6c2004722d40de41608b0ef899ed0a9ebdced6
Andrea Pera <perozzo92@hotmail.it>
2018-06-17 11:27:42 +0200

    Aggiunto la modal per il pulsante "who are we?" nella home del Customer.

--- app/templates/Customer/home/homeCustomerController.js
+++ app/templates/Customer/home/homeCustomerController.js
@@ -2,7 +2,33 @@
 
 angular.module('ticketsystem.homeCustomer', ['ngRoute'])
 
-    .controller('homeCustomerCtrl', function ($scope, restService, httpService,$location,storageService) {
+    .controller('homeCustomerCtrl', function ($scope, $modal, $log) {
 
+        var modalInstance;
+
+        $scope.whoAreWe = function () {
+
+            modalInstance = $modal.open({
+                templateUrl: '/modal/modal-who-are-we.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                backdrop: 'static',
+            });
+
+            modalInstance.result.then(function () {
+
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        }
 
     });
+
+
+var ModalInstanceCtrl = function ($scope, $modalInstance) {
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+};
