4acc7b592ec21f70baa0ec11ef120781bbfe5513
Andrea Pera <perozzo92@hotmail.it>
2018-06-29 22:10:17 +0200

    Implementati i pulsanti del footer delle Home: Terms of use, Privacy Regulations e FAQ.

--- app/templates/common/home/home.js
+++ app/templates/common/home/home.js
@@ -20,6 +20,54 @@ angular.module('ticketsystem.home', ['ngRoute'])
             }, function () {
                 $log.info('Modal dismissed at: ' + new Date());
             });
+        };
+
+        $scope.termsOfUse = function () {
+
+            modalInstance = $modal.open({
+                templateUrl: '/modal/modal-terms-of-use.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                backdrop: 'static',
+            });
+
+            modalInstance.result.then(function () {
+
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $scope.privacyRegulations = function () {
+
+            modalInstance = $modal.open({
+                templateUrl: '/modal/modal-privacy-regulations.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                backdrop: 'static',
+            });
+
+            modalInstance.result.then(function () {
+
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $scope.faq = function () {
+
+            modalInstance = $modal.open({
+                templateUrl: '/modal/modal-faq.html',
+                controller: ModalInstanceCtrl,
+                scope: $scope,
+                backdrop: 'static',
+            });
+
+            modalInstance.result.then(function () {
+
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
         }
 
     });
